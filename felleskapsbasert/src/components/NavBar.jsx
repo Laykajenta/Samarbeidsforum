019774bdeb34e0161f37f2f2326409b765871fcb
Laykajenta <96973776+Laykajenta@users.noreply.github.com>
2026-04-28 06:10:26 +0200

    slettet bilder som ikke brukes

---
+++ felleskapsbasert/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import logo from "../assets/logo_samarbeidsforum.jpg";
+
+export default function NavBar() {
+  return (
+    <nav id="main-nav">
+      <a href="#" className="nav-logo" aria-label="Samarbeidsforum – hjem">
+        <img src={logo} alt="Samarbeidsforum logo" />
+      </a>
+
+      <ul className="nav-links">
+        <li><a href="#" className="active">Forside</a></li>
+        <li><a href="#nyheter">Nyheter</a></li>
+        <li><a href="#arrangementer">Arrangementer</a></li>
+        <li><a href="#medlemmer">Medlemmer</a></li>
+        <li><a href="#kontakt">Kontakt</a></li>
+      </ul>
+    </nav>
+  );
+}
